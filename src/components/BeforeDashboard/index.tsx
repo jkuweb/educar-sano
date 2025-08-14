f769eb242359a858db74b6d896274def1bc44df6
Joseba <joseba@debian.localhost>
2025-08-14 19:28:32 +0200

    update  BeforeDasboard component

--- src/components/BeforeDashboard/index.tsx
+++ src/components/BeforeDashboard/index.tsx
@@ -1,7 +1,27 @@
+import { Banner } from '@payloadcms/ui/elements/Banner'
 import './index.scss'
 
+const baseClass = 'before-dashboard'
+
 const BeforeDashboard: React.FC = () => {
-  return <h4>¡Bienvenido a tu panel de control!</h4>
+  return (
+    <div className="baseClass">
+      <Banner className={`${baseClass}__banner`} type="success">
+        <h4>🖐 ¡Bienvenido a tu panel de control!</h4>
+      </Banner>
+      <p>Puedes empezar por:</p>
+      <ul className={`${baseClass}__instructions`}>
+        <li>
+          <a href="/" target="_blank">
+            Visitar tu página web
+          </a>
+        </li>
+        <li>
+          <a href="mailto:contacto@josebaquerejeta.com">Realizar una consulta</a>
+        </li>
+      </ul>
+    </div>
+  )
 }
 
 export default BeforeDashboard
